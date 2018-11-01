1293cf6e99d1a1c0063d1e20b6de3fcaae958b7a
Mike Conley <mike.d.conley@gmail.com>
2018-11-01 13:33:46 -0400

    Add ESLint and some linting aliases.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2
+        ],
+        "quotes": [
+            2,
+            "double"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "extends": "eslint:recommended"
+};
