mikeconley/myqonly

commit 8d7a8cd07ed0ad3c5310d7bb68c334189053fb3b

Add 'ignoredRepos' option to ignore specific repositories with group requests

addon/background.js
@@ -420,7 +420,13 @@ var MyQOnly = {
         .map(s => s.trim())
         .filter(Boolean));
 
-    if (ignoredTeams.size === 0) {
+    let ignoredRepos = new Array(
+      (settings.ignoredRepos || "")
+        .split(",")
+        .map(s => s.trim())
+        .filter(Boolean));
+
+    if (ignoredTeams.size === 0 && ignoredRepos.length === 0) {
       return { reviewTotal: data.total_count, };
     }
     // Sadly, `-team-review-requested:` doesn't appear to work in the API, so we
@@ -457,7 +463,8 @@ var MyQOnly = {
       // If review was requested directly, always treat as a valid PR.
       if (reviewers.some(reviewer => reviewer.login === username)) {
         validPrs++;
-      } else if (teams.every(team => !ignoredTeams.has(team.name))) {
+      } else if (teams.every(team => !ignoredTeams.has(team.name)) &&
+                 ignoredRepos.every(repo => !prUrl.includes(repo))) {
         validPrs++;
       }
     }

addon/content/options/options.html
@@ -58,6 +58,8 @@
     </div>
     <label for="github-ignored-teams">Comma-separated list of github teams to ignore.</label>
     <input type="text" id="github-ignored-teams" data-setting="ignoredTeams">
+    <label for="github-ignored-repos">Comma-separated list of github repositories to ignore as a substring of the url (only applies when requested as part of a team).</label>
+    <input type="text" id="github-ignored-repos" data-setting="ignoredRepos">
   </div>
 </section>
 

addon/content/options/options.js
@@ -89,6 +89,10 @@ const Options = {
     let ignoredTeams =
       githubSettings.querySelector("[data-setting='ignoredTeams']");
     ignoredTeams.value = service.settings.ignoredTeams || "";
+
+    let ignoredRepos =
+      githubSettings.querySelector("[data-setting='ignoredRepos']");
+    ignoredRepos.value = service.settings.ignoredRepos || "";
   },
 
   onUpdateService(event, serviceType) {

commit 822491976467ea886a64ea7be0297e7c1b733137

Fix regression breaking Github reviews
This is a regression from PR #52

addon/background.js
@@ -425,8 +425,7 @@ var MyQOnly = {
         .map(s => s.trim())
         .filter(Boolean));
 
-    let ignoredRepos = new Array(
-      (settings.ignoredRepos || "")
+    let ignoredRepos = (settings.ignoredRepos || "")
         .split(",")
         .map(s => s.trim())
         .filter(Boolean));